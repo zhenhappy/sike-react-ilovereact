ebe7aec8da5f9117bd9e0742805ce6deb2a5e653
zhenhappy <q505507538@gmail.com>
2016-01-16 20:45:32 +0800

    任务四完成

--- js/app.js
+++ js/app.js
@@ -26,15 +26,15 @@ function animateRobot() {
 // 更新侧边栏原点
 function updateSliderControl() {
   // 获得所有的 slider 链接
-  var links = document.querySelectorAll("#slider-control a")
+  var links = document.querySelectorAll("#slider-control a");
 
   for(var i = 0; i < links.length; i++) {
     var link = links[i];
 
     // 获取被链接指向的部分
     var section = document.querySelector(link.getAttribute('href'));
-    var sectionTop = section.getBoundingClientRect().top  + ( window.pageYOffset || section.scrollTop )  - ( section.clientTop  || 0 )
-    var sectionBottom = section.getBoundingClientRect().left + ( window.pageXOffset || section.scrollLeft ) - ( section.clientLeft || 0 );
+    var sectionTop = section.getBoundingClientRect().top  + ( window.pageYOffset || section.scrollTop )  - ( section.clientTop  || 0 );
+    var sectionBottom = section.getBoundingClientRect().bottom + ( window.pageYOffset || section.scrollTop )  - ( section.clientBottom  || 0 );
 
     // 检查 window.scrollY 是否在这部分中
     if(window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
@@ -44,17 +44,40 @@ function updateSliderControl() {
     }
   }
 }
+
+
+function scrollToElement(element) {
+  var topOfElement = element.getBoundingClientRect().top  + ( window.pageYOffset || element.scrollTop )  - ( element.clientTop  || 0 );
+  TweenLite.to(window, 2, {
+    scrollTo: {
+      y: topOfElement
+    },
+    ease: Power2.easeOut
+  });
+}
+
+function addSmoothScrolling() {
+  var links = document.querySelectorAll("#slider-control a");
+
+  for(var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.addEventListener('click', function() {
+      // `event` 是鼠标点击事件
+      event.preventDefault();
+      // BUG 警告！使用闭包或者 ES6 `let` 修复。
+      var href = this.getAttribute('href');
+      scrollToElement(document.querySelector(href));
+    });
+  }
+}
+
 window.onload = function() {
   animateLogo();
   animateRobot();
+  updateSliderControl();// 当页面首次加载的时候更新 slider
+  addSmoothScrolling();
 };
-// // 使用 onscroll 回调函数来更新 slider
+// 使用 onscroll 回调函数来更新 slider
 window.onscroll = function() {
-  // ...
   updateSliderControl();
 }
-// // 当页面首次加载的时候更新 slider
-window.onload = function() {
-  // ...
-  updateSliderControl();
-};
